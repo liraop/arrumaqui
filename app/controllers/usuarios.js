24cc447a249ae7f291f4805118e1524005b7c814
liraop <pedrolira@live.com>
2018-08-14 11:03:36 -0300

    removing unnecessarymethods and routes

--- app/controllers/usuarios.js
+++ app/controllers/usuarios.js
@@ -14,26 +14,6 @@ module.exports = function (app) {
     var controller = {};
     var ad = new ActiveDirectory(config);
 
-    //Função que salva o usuario no bd
-    controller.salvaUsuario = (req, res) => {
-        console.log('API: salvaUsuario');
-        var hashedPassword = bcrypt.hashSync(req.body.senha, 8);
-        var usuario = new Usuario(req.body);
-
-        usuario.senha = hashedPassword;
-
-        usuario.save(function (erro, usuario) {
-            if (erro) {
-                res.status(500).json(erro).end();
-                console.log(erro);
-            } else {
-                res.json(usuario);
-                res.status(201);
-                console.log(201);
-            }
-        });
-    };
-
     controller.atualizaUsuarioPorId = (req, res) => {
         console.log('API: atualizaUsuarioPorId');
         let _idUsuario = req.params.id;
@@ -100,22 +80,6 @@ module.exports = function (app) {
             );
     };
 
-    //Função que lista Todos os usuarios do bd
-    controller.listaTodos = function (req, res) {
-        console.log('API: listaTodos');
-        Usuario.find().exec().then(
-            function (usuario) {
-                res.json(usuario);
-                res.status(201);
-                console.log(201);
-            },
-            function (erro) {
-                console.error(erro);
-                res.status(500).json(erro);
-            }
-        );
-    };
-
     //Função que retorna um usuario pelo email
     controller.obtemUsuarioComEmail = function (req, res) {
         console.log('API: obtemUsuarioComEmail');
@@ -131,23 +95,6 @@ module.exports = function (app) {
                 function (erro) {
                     console.log(erro);
                     res.status(404).json(erro);
-                }
-            );
-    };
-
-    //Função que remove um usuario
-    controller.removeUsuario = (req, res) => {
-        console.log('API: removeUsuario');
-
-        let _emailUsuario = req.params.email;
-        let criterio = { "contato.email": _emailUsuario };
-        Usuario.remove(criterio).exec()
-            .then(
-                function () {
-                    res.end();
-                },
-                function (erro) {
-                    return console.error(erro);
                 }
             );
     };
@@ -183,22 +130,6 @@ module.exports = function (app) {
             }
         });
     }
-
-    // Listagem a retornar da API
-    controller.listaServicos = (req, res) => {
-        Usuario.find({}, (erro, usuarios) => {
-
-            if (erro) return res.status(400).json(erro);
-
-            const servicos = usuarios.reduce((listagem, pessoa) => {
-                const servicosPessoa = mapPessoa(pessoa);
-                return listagem.concat(servicosPessoa);
-            }, []);
-
-            res.json(servicos);
-
-        });
-    };
 
     //Autentica Login
     controller.autenticaLogin = (req, res, next) => {
--- app/routes/usuarios.js
+++ app/routes/usuarios.js
@@ -3,14 +3,9 @@ module.exports = function (app) {
 
     let controller = app.controllers.usuarios;
 
-    app.route('/api/usuarios')
-        .post(controller.salvaUsuario)
-        .get(controller.listaTodos);
-
     app.route('/api/usuarios/getEmail/:email')
         .post(controller.autenticaUsuario)
         .get(controller.obtemUsuarioComEmail)
-        .delete(controller.removeUsuario);
 
     app.route('/api/usuarios/email')
         .post(controller.autenticaUsuario);
@@ -18,9 +13,6 @@ module.exports = function (app) {
     app.route('/api/usuarios/:id')
         .get(controller.obtemUsuarioPorId)
         .post(controller.atualizaUsuarioPorId);
-
-    app.route('/api/servicos')
-        .get(controller.listaServicos);
 
     app.route('/api/login')
         .post(controller.autenticaLogin);
